ff5bff33fefdb26f7311a4137d932db2b33bdbd3
Muhammad Auliya Dimas Prasetiyo <79791987+prasetiyo08@users.noreply.github.com>
2025-05-13 06:43:03 +0700

    Hapus semua file kecuali folder edubuild

--- edubuild-backend/middleware/upload.js
+++
@@ -1,8 +0,0 @@
-// Placeholder for upload.js
-const multer = require('multer');
-
-// Gunakan memory storage agar bisa langsung dikirim ke Firebase Storage
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-module.exports = upload;
