998e2564822fb5739a66e19c3345f091ebd00333
Muhammad Auliya Dimas Prasetiyo <79791987+prasetiyo08@users.noreply.github.com>
2025-05-15 21:10:15 +0700

    delete

--- edubuild-backend/utils/score.js
+++
@@ -1,9 +0,0 @@
-function calculateScore(report) {
-  let score = 100;
-  if (report.roofLeak) score -= 30;
-  if (report.brokenToilet) score -= 20;
-  // Tambahkan logika lainnya sesuai kebutuhan
-  return score;
-}
-
-module.exports = calculateScore;
